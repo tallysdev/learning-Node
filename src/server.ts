386a53ca1c6f7cac9cc5d202aa5b90a9a6540a5b
Tallys-aureliano <tallysaureliano@outlook.com>
2022-12-19 18:33:46 -0300

    config routes

--- src/server.ts
+++ src/server.ts
@@ -9,18 +9,19 @@ app.use(express.json());
 
 app.use(routes);
 
-app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+app.use(
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppErros) {
         return response.status(err.statusCode).json({
             status: "error",
-            message: err.message
-        })
+            message: err.message,
+        });
     }
 
     return response.status(500).json({
         status: "error",
-        message: 'Internal server error - ${err.message}'
-    })
+        message: `Internal server error - ${err.message}`,
+    });
 });
 
 app.listen(3333, () => console.log("Server is running in port 3333"));
